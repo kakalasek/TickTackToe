ba1577af3e0421606ad38115ce679942eeff70dc
Typicek <carotkacz333@seznam.cz>
2023-06-14 10:58:21 +0200

    It just works

--- script.js
+++ script.js
@@ -9,20 +9,19 @@ let boxVector = [[boxes.item(0), 0],
                  [boxes.item(6), 0],
                  [boxes.item(7), 0],
                  [boxes.item(8), 0]]
-let turn = 0
 
 function activateBox(){
 
-    if(turn == 0 && boxVector[this.id][1] == 0){ boxes.item(this.id).setAttribute('style', 'background-color: blue;'); boxVector[this.id][1] = 1;}
-    else if (turn == 1 && boxVector[this.id][1] == 0){ boxes.item(this.id).setAttribute('style', 'background-color: red;'); boxVector[this.id][1] = 2;}
-
-    turn = !turn
+    if(boxVector[this.id][1] == 0){ boxes.item(this.id).setAttribute('style', 'background-color: blue;')
+                                    boxVector[this.id][1] = 1;
+                                    enemyTurn()
+                                }
     winCheck()
 }
 
 function winCheck(){
-    if(bruteforcewin(1) == 1){ document.getElementById('winlabel').setAttribute('style', 'color: blue;'); lock()}
-    else if(bruteforcewin(2) == 1){ document.getElementById('winlabel').setAttribute('style', 'color: red;'); lock()}
+    if(bruteforcewin(1) == 1){ document.getElementById('winlabel').setAttribute('style', 'color: blue; visibility: visible;'); lock()}
+    else if(bruteforcewin(2) == 1){ document.getElementById('winlabel').setAttribute('style', 'color: red; visibility: visible;'); lock()}
 }
 
 function bruteforcewin(player){
@@ -41,4 +40,21 @@ function lock(){
     for(let i = 0; i < 9; i++){
         boxVector[i][1] = 3
     }
+}
+
+function generate_random(){
+    return Math.floor(Math.random() * 8)
+}
+function enemyTurn(){
+    let ct = 0
+    for(let i = 0; i < 9; i++){
+        if(boxVector[i][1] != 0) ct++
+        if(ct == 9) return
+    }
+    let id
+    do{
+        id = generate_random()
+    }while(boxVector[id][1] != 0);
+    setTimeout(() => boxes.item(id).setAttribute('style', 'background-color: red;'), 100)
+    boxVector[id][1] = 2
 }
